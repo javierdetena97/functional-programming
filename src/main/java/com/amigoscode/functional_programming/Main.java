025280079a6db401e1d0441685b90ead672732d3
Javier de Tena <javidetena97@gmail.com>
2024-09-24 16:32:14 +0200

    Using Predicate

--- src/main/java/com/amigoscode/functional_programming/Main.java
+++ src/main/java/com/amigoscode/functional_programming/Main.java
@@ -3,12 +3,23 @@ package com.amigoscode.functional_programming;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Functional Programming Course from Amigoscode");
+        String email = "hello@amigoscode.com";
+        System.out.println(isValidEmail(email));
+        System.out.println(isValidEmailPredicate.test(email));
+    }
+
+    // Functional approach
+    private static Predicate<String> isValidEmailPredicate = email -> email.contains("@");
+
+    // Imperative approach
+    private static boolean isValidEmail(String email) {
+        return email.contains("@");
     }
 
 }
