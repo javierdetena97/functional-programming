efd9f9c4a6ea70ff108fa7ae3e1b0d35af74eeee
Javier de Tena <javidetena97@gmail.com>
2024-09-24 17:09:25 +0200

    Using Supplier

--- src/main/java/com/amigoscode/functional_programming/Main.java
+++ src/main/java/com/amigoscode/functional_programming/Main.java
@@ -2,11 +2,25 @@ package com.amigoscode.functional_programming;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Functional Programming Course from Amigoscode");
+        System.out.println(getUrlSupplier.get());
+    }
+
+    // Functional approach
+    private static Supplier<List<String>> getUrlSupplier = () -> {
+        // logic
+        return List.of("https://amigoscode.com");
+    };
+
+    // Imperative approach
+    private static String getUrl() {
+        return "https://amigoscode.com";
     }
 
 }
